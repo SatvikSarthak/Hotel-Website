977d988967041b33b508e0cb5deaf33451830fc6
Satvik Sarthak <satvik.sarthak17@gmail.com>
2024-06-16 23:08:03 +0530

    developed the project further.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+// swiper slider
+var swiper = new Swiper(".mySwiper", {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
