c9ea40375edecfb970dd52d405bb9c6da6d03feb
Satvik Sarthak <satvik.sarthak17@gmail.com>
2024-06-17 01:59:06 +0530

    updation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 
-
+//nav-hide
+var navBar= document.querySelectorAll(".nav-link");
+var navBarCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+               navBarCollapse.classList.remove("show");
+    })
+})
 
 // swiper slider
 var swiper = new Swiper(".mySwiper", {
