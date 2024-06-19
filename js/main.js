f5f1d0ab2729aac55572db93ca8e9bd8e82ee6f8
Satvik Sarthak <satvik.sarthak17@gmail.com>
2024-06-19 18:25:55 +0530

    development

--- js/main.js
+++ js/main.js
@@ -20,3 +20,21 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+document.addEventListener("DOMContentLoaded",()=>{
+    function counter(id,start,end,duration){
+        let obj= document.getElementById(id),
+        current=start,
+        range= end-start,
+        increment = end > start ? 1 : -1,
+        step= Math.abs(Math.floor(duration/range)),
+        timer= setInterval(()=>{
+            current += increment;
+            obj.textContent=current;
+            if(current==end) clearInterval(timer);
+        },step);
+    }
+    counter("count1",0,956,3000);
+    counter("count2",0,1256,1400);
+    counter("count3",0,3456,2500);
+    counter("count4",0,2006,3500);
+})
